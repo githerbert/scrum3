02fc71dd79846e3e2091e282012f2ca7ca401906
K <K@192.168.2.100>
2014-12-10 15:57:47 +0100

    Kati: Suchfelder usw. für Backlog und Bugs eingebaut

--- Scrum3/src/de/scrummies/gui/Main.java
+++ Scrum3/src/de/scrummies/gui/Main.java
@@ -42,6 +42,7 @@ import javax.swing.event.ChangeEvent;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
+import javax.swing.JComboBox;
 
 public class Main extends JFrame implements ActionListener
 {
@@ -63,6 +64,12 @@ public class Main extends JFrame implements ActionListener
 	private JMenuItem prioUpdate;
 	
 	private JTabbedPane tabbedPane;
+	private JTextField searchField;
+	private JTextField txtHierSummeStorypoints;
+	
+	private JComboBox filter_bl;
+	private JTextField textField;
+	private JComboBox filter_bugs;
 
 	/**
 	 * Create the frame.
@@ -198,8 +205,110 @@ public class Main extends JFrame implements ActionListener
 		bl= new Tabelle("Story Points",new FindAllUserStorys());
 		tabbedPane.addTab("Backlog", null, bl, null);
 		
+		//Gehört zum Backlog und bleibt daher stehen
+		JPanel blSearch_panel = new JPanel();
+		bl.add(blSearch_panel, BorderLayout.NORTH);
+		GridBagLayout gbl_blSearch_panel = new GridBagLayout();
+		gbl_blSearch_panel.columnWidths = new int[]{44, 165, 75, 69, 106, -307, 0};
+		gbl_blSearch_panel.rowHeights = new int[]{36, 0};
+		gbl_blSearch_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_blSearch_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		blSearch_panel.setLayout(gbl_blSearch_panel);
+		
+		JLabel lblSuche = new JLabel("Suche");
+		GridBagConstraints gbc_lblSuche = new GridBagConstraints();
+		gbc_lblSuche.insets = new Insets(0, 0, 0, 5);
+		gbc_lblSuche.anchor = GridBagConstraints.EAST;
+		gbc_lblSuche.gridx = 0;
+		gbc_lblSuche.gridy = 0;
+		blSearch_panel.add(lblSuche, gbc_lblSuche);
+		
+		searchField = new JTextField();
+		GridBagConstraints gbc_searchField = new GridBagConstraints();
+		gbc_searchField.anchor = GridBagConstraints.WEST;
+		gbc_searchField.insets = new Insets(0, 0, 0, 5);
+		gbc_searchField.gridx = 1;
+		gbc_searchField.gridy = 0;
+		blSearch_panel.add(searchField, gbc_searchField);
+		searchField.setColumns(30);
+		
+		JLabel lblSumme = new JLabel("Story Points");
+		GridBagConstraints gbc_lblSumme = new GridBagConstraints();
+		gbc_lblSumme.insets = new Insets(0, 0, 0, 5);
+		gbc_lblSumme.anchor = GridBagConstraints.EAST;
+		gbc_lblSumme.gridx = 2;
+		gbc_lblSumme.gridy = 0;
+		blSearch_panel.add(lblSumme, gbc_lblSumme);
+		
+		txtHierSummeStorypoints = new JTextField();
+		GridBagConstraints gbc_txtHierSummeStorypoints = new GridBagConstraints();
+		gbc_txtHierSummeStorypoints.insets = new Insets(0, 0, 0, 5);
+		gbc_txtHierSummeStorypoints.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtHierSummeStorypoints.gridx = 3;
+		gbc_txtHierSummeStorypoints.gridy = 0;
+		blSearch_panel.add(txtHierSummeStorypoints, gbc_txtHierSummeStorypoints);
+		txtHierSummeStorypoints.setColumns(10);
+		
+		JLabel lblFilter = new JLabel("Status-Filter");
+		GridBagConstraints gbc_lblFilter = new GridBagConstraints();
+		gbc_lblFilter.insets = new Insets(0, 0, 0, 5);
+		gbc_lblFilter.anchor = GridBagConstraints.EAST;
+		gbc_lblFilter.gridx = 4;
+		gbc_lblFilter.gridy = 0;
+		blSearch_panel.add(lblFilter, gbc_lblFilter);
+		
+		filter_bl = new JComboBox(new String[]{"All","Approved","Done","New"}); //Vollständig?
+		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
+		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox_1.gridx = 5;
+		gbc_comboBox_1.gridy = 0;
+		blSearch_panel.add(filter_bl, gbc_comboBox_1);
+		
+		
 		bug = new Tabelle("Kritikalität","");
 		tabbedPane.addTab("Bugliste", null, bug, null);
+		
+		//Gehört zum Panel der Bugliste
+		JPanel bugSearch_panel = new JPanel();
+		bug.add(bugSearch_panel, BorderLayout.NORTH);
+		GridBagLayout gbl_bugSearch_panel = new GridBagLayout();
+		gbl_bugSearch_panel.columnWidths = new int[]{43, 0, 0, 0, 18, 128, 0, 0};
+		gbl_bugSearch_panel.rowHeights = new int[]{37, 0};
+		gbl_bugSearch_panel.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_bugSearch_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		bugSearch_panel.setLayout(gbl_bugSearch_panel);
+		
+		JLabel label = new JLabel("Suche");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.anchor = GridBagConstraints.EAST;
+		gbc_label.insets = new Insets(0, 0, 0, 5);
+		gbc_label.gridx = 0;
+		gbc_label.gridy = 0;
+		bugSearch_panel.add(label, gbc_label);
+		
+		textField = new JTextField();
+		textField.setColumns(30);
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 0, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		bugSearch_panel.add(textField, gbc_textField);
+		
+		JLabel label_2 = new JLabel("Status-Filter");
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.anchor = GridBagConstraints.EAST;
+		gbc_label_2.insets = new Insets(0, 0, 0, 5);
+		gbc_label_2.gridx = 5;
+		gbc_label_2.gridy = 0;
+		bugSearch_panel.add(label_2, gbc_label_2);
+		
+		filter_bugs = new JComboBox(new Object[]{});
+		GridBagConstraints gbc_combo_bugs = new GridBagConstraints();
+		gbc_combo_bugs.fill = GridBagConstraints.HORIZONTAL;
+		gbc_combo_bugs.gridx = 6;
+		gbc_combo_bugs.gridy = 0;
+		bugSearch_panel.add(filter_bugs, gbc_combo_bugs);
 
 	}
 
